6db8c0517eeb7f11c4e66011fcaf0a4699e913b8
bengroves <bengroves15@gmail.com>
2020-10-22 21:07:57 -0400

    commit tree 09d3243c60d3477102378fd57a6dedc7034d9f8f

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+def prime?(num)
+    num = num.abs()
+    list1 = (1..num).to_a
+    bin1 = []
+    bin2 = []
+    i = 0
+    for i in list1
+        factor = num % list1[i-1]
+        if factor >= 1
+            bin1.append(factor)
+        else
+            bin2.append(factor)
+        end 
+    end 
+
+    if num == 2 || num == -2
+        return true
+    elsif num == 1 || num == -1
+        return false
+    elsif bin2.length == 2
+        return true
+    end 
+  end
